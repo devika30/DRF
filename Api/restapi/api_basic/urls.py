d1a5d8daaf7a474c12efa2ce2706b2c11649be47
Devika Shailesh Olkar <devikaolkar@kccemsr.edu.in>
2021-02-02 18:29:52 +0530

    updated the views

--- Api/restapi/api_basic/urls.py
+++ Api/restapi/api_basic/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from .views import article_list 
+from .views import *
 urlpatterns = [
    path('article/',article_list),
+   path('detail/<int:pk>/',article_detail)
 ]
